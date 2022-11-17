ace67ea4d7fc50deb9da84f3e79b2d276a651ca1
lmiel <lucia.mielgo@gmail.com>
2022-11-17 22:59:32 +0100

    actualizado

--- juego.py
+++ juego.py
@@ -1,64 +1,48 @@
 import random
+import entrada as en
+import juego as jg
 
-def menu():
-    print("ADIVINA EL NUMERO")
-    print("-----------------")
-    print("1: Nivel Simple (Entre 0 y 100)")
-    print("2: Nivel Intermedio (Entre 0 y 1000)")
-    print("3: Nivel Avanzado (Entre 0 y 1000000)")
-    print("4: Nivel Experto (Entre 0 1000000000000)")
-    print("5: Salir")
+def scoreBoard(puntuacion, records):
+    userName = input("Introduzca Usuario: ")
+    records.append(str(userName) + " " + str(puntuacion))
 
-def niveles():
+def play(randomNum, maximo, intentosMax, records):
+    
+    limitemin = 0
+    limitemax = maximo
+
+    intentos = 0
+
     while True:
-        nivel=input("Introduzca el nivel de dificultad: ")
-        nivel=int(nivel)
-        if nivel == 1:
-            return 99
-            break
-        elif nivel == 2:
-            return 999
-            break
-        elif nivel == 3:
-            return 9999
-            break
-        elif nivel == 4:
-            return 999999999999
-            break
-        elif nivel == 5:
-            break
+        en.ayuda(limitemin, limitemax)
+        numberGuessed = int(input("Introduzca numero entre 0 y " + str(maximo) + " incluidos: "))
 
-def limites(maximo):
-    numero = random.randint(0, maximo)
-    return numero
-    print(numero)
+        intentos = intentos + 1
 
-def juego():
-    while True:
-
-        intento = input("Introduzca numero entre 0 y " + str(maximo) + " incluidos: ")
-        intento = int(intento)
-
-        if intento < numero:
+        if numberGuessed < randomNum:
             print("El numero es demasiado pequeño")
-        elif intento > numero:
+        elif numberGuessed > randomNum:
             print("El numero es demasiado grande")
         else:
             print("¡Has acertado!")
+            scoreBoard(intentos, records)
+            break
+        
+        if intentos > intentosMax:
+            print("Demasiados intentos")
             break
 
-def ayuda(intento):
+        limitemin, limitemax = nuevoslim(numberGuessed, randomNum, limitemin, limitemax)
 
-    ayuda = input("¿Quieres una pista?(s/n): ")
-    
-    if ayuda == "s":
-        if intento < numero:
-            minimo = intento
-        elif intento > numero:
-            maximo = intento
+def nuevoslim(numberGuessed, randomNum, limitemin, limitemax):
+    if numberGuessed < randomNum:
+        if numberGuessed > limitemin:
+            return numberGuessed, limitemax
+    else:
+        if numberGuessed < limitemax:
+            return limitemin, numberGuessed
+    return limitemin,limitemax
 
-
-menu()
-maximo=niveles()        
-numero=limites(maximo)
-juego()
+def getRandomNum(maximo):
+    numero = random.randint(0, maximo)
+    return numero
